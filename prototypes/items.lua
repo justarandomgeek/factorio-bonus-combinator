dd2c9428323c2973f0cb0bca32639bc0003a942e
Thomas Deal <justarandomgeek@gmail.com>
2017-12-13 10:32:13 -0500

    Update for 0.16

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,6 +3,7 @@ data:extend{
     type = "item",
     name = "bonus-combinator",
     icon = "__base__/graphics/icons/constant-combinator.png",
+    icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "circuit-network",
     place_result="bonus-combinator",
